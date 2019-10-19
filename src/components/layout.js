b6f123eef2107f9fa0df22bd582cad1b3e96a59c
yuyaebean <yuyaebean@gmail.com>
2019-10-20 02:25:25 +0900

    4. Data in Gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,50 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import Container from './container'
+import { css } from '@emotion/core'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { rhythm } from '../utils/typography'
 
-const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+export default ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
 
-export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    <header style={{ marginBottom: `1.5rem` }}>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
       </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children}
-    <footer>
-      <Container>
-        <p style={{ color: `rgb(114, 125, 134)`, fontSize: `14px`}}>
-          © 2019, TARRAZU All rights reserved.
-        </p>
-      </Container>
-    </footer>
-  </div>
-)
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
+}
