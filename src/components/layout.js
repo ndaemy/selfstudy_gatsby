390f8f3fbf0e03b7ec76d8f4bb41770de28edf5f
yuyaebean <yuyaebean@gmail.com>
2019-10-20 01:27:39 +0900

    3. Creating Nested Layout Components

---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Container from './container'
+
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default ({ children }) => (
+  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>
+    {children}
+    <footer>
+      <Container>
+        <p style={{ color: `rgb(114, 125, 134)`, fontSize: `14px`}}>
+          © 2019, TARRAZU All rights reserved.
+        </p>
+      </Container>
+    </footer>
+  </div>
+)
